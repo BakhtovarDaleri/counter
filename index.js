035801f0e72b6e88fe3f9d17a7258f3e7845fdd2
Bakhtovar Daleri <bakhtovarrahmatov@mail.ru>
2024-09-25 09:07:37 +0300

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+let count = 0; // Глобальная переменная
+
+// Вызываем функцию, которая увеличивает count
+function incrementCount() {
+  count++;
+  console.log(count);
+}
+
+incrementCount();
+incrementCount();
+incrementCount();
+incrementCount();
